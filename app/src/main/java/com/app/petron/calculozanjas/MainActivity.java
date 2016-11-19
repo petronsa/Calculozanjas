79cbbf28da1ac19307b00f42b0d35d0a4c271cfa
petronsa <androidtorrejoncillo1@gmail.com>
2016-11-19 21:08:56 +0100

    Con publi, casi traducida y con resoluciones

--- app/src/main/java/com/app/petron/calculozanjas/MainActivity.java
+++ app/src/main/java/com/app/petron/calculozanjas/MainActivity.java
@@ -3,14 +3,19 @@ package com.app.petron.calculozanjas;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import com.startapp.android.publish.StartAppAd;
+import com.startapp.android.publish.StartAppSDK;
 
 public class MainActivity extends AppCompatActivity {
-
+    private StartAppAd startAppAd = new StartAppAd(this);
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        StartAppSDK.init(this, "101423750", "210265439", true);
         setContentView(R.layout.activity_main);
 
         Button zanja_normal = (Button) findViewById(R.id.boton_zanja_normal);
@@ -32,5 +37,43 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+    }
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+
+
+        //poner en marcha la publicidad al salir
+        MainActivity.this.startAppAd.showAd();
+        MainActivity.this.startAppAd.loadAd();
+
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        startAppAd.onPause();
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        switch (item.getItemId()) {
+
+
+            case R.id.fuera:
+                //botón salir
+                MainActivity.this.startAppAd.showAd();
+                MainActivity.this.startAppAd.loadAd();
+
+                finish();
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
