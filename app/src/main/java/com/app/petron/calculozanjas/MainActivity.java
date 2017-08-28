60f7a5ec7fbd377fe19b869e254f0529ce4573af
Manuel <androidtorrejoncillo1@gmail.com>
2017-08-28 13:13:43 +0200

    Con publi, casi traducida y con resoluciones, corregido un fallo con los decimales y publicada la version 1.6

--- app/src/main/java/com/app/petron/calculozanjas/MainActivity.java
+++ app/src/main/java/com/app/petron/calculozanjas/MainActivity.java
@@ -1,14 +1,15 @@
 package com.app.petron.calculozanjas;
 
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
-import com.startapp.android.publish.StartAppAd;
-import com.startapp.android.publish.StartAppSDK;
+
+import com.startapp.android.publish.adsCommon.StartAppAd;
+import com.startapp.android.publish.adsCommon.StartAppSDK;
 
 public class MainActivity extends AppCompatActivity {
     private StartAppAd startAppAd = new StartAppAd(this);
